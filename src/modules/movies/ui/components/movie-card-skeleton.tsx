be259891056250531a12218bb9fef950fee8cd82
enriquedlc <enriquedlc2d@gmail.com>
2025-06-10 13:50:03 +0100

    :lipstick: movie card and skeleton styles

--- src/modules/movies/ui/components/movie-card-skeleton.tsx
+++ src/modules/movies/ui/components/movie-card-skeleton.tsx
@@ -1,13 +1,19 @@
 import styles from "./movie-card.module.css";
 
-interface Props {
+interface MovieCardSkeletonProps {
   orientation?: "vertical" | "horizontal";
 }
 
-export function MovieCardSkeleton({ orientation = "vertical" }: Props) {
+export function MovieCardSkeleton({
+  orientation = "vertical",
+}: MovieCardSkeletonProps) {
   const cardClassName = `${styles.card} ${
     orientation === "horizontal" ? styles.horizontal : styles.vertical
   } ${styles.skeleton}`;
 
-  return <div className={cardClassName}></div>;
+  return (
+    <div className={cardClassName}>
+      <div className={styles.imagePlaceholder} />
+    </div>
+  );
 }
