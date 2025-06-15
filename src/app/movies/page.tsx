ae0a44a7a0b0cb23bb09634d7ee831befe9dd558
enrique <enriquedlc.dev@gmail.com>
2025-06-15 03:57:27 +0100

    :package:

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { createGenreRepositoryApi } from "@/modules/movies/infrastructure/genre-repository-api";
 import { createMovieRepositoryApi } from "@/modules/movies/infrastructure/movie-repository-api";
 import { createTokenRepositoryCookies } from "@/shared/token/infrastructure/token-repository-cookies";
@@ -12,6 +13,8 @@ export default async function MoviesPage() {
     genreRepository.getAll(),
     movieRepository.getAll(),
   ]);
+
+  if (!genres || !movies) return notFound();
 
   return (
     <>
